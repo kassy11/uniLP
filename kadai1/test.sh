560b1b5f3df1ee373b0d5ad812a595cdb377bc43
kassy11 <kotaro11kassy@gmail.com>
2020-10-26 15:00:29 +0900

    add kadai1 test.sh

---
+++ kadai1/test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+./tc kadai1_samples/sample011.mpl >> result/sample011.txt
+./tc kadai1_samples/sample11.mpl >> result/sample11.txt
+./tc kadai1_samples/sample11p.mpl >> result/sample11p.txt
+./tc kadai1_samples/sample11pp.mpl >> result/sample11pp.txt 
+
+./tc kadai1_samples/sample12.mpl >> result/sample12.txt
+./tc kadai1_samples/sample13.mpl >> result/sample13.txt
+
+./tc kadai1_samples/sample014.mpl >> result/sample014.txt
+./tc kadai1_samples/sample14.mpl >> result/sample14.txt
+./tc kadai1_samples/sample14p.mpl >> result/sample14p.txt
+
+./tc kadai1_samples/sample15.mpl >> result/sample15.txt
+./tc kadai1_samples/sample15a.mpl >> result/sample15a.txt
+
+./tc kadai1_samples/sample16.mpl >> result/sample16.txt
+./tc kadai1_samples/sample17.mpl >> result/sample17.txt
+./tc kadai1_samples/sample18.mpl >> result/sample18.txt
+./tc kadai1_samples/sample19p.mpl >> result/sample19p.txt
+
+
+
